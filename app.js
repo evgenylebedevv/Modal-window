54f7b04947ee4a77ad9eec160d2b443a1d01ca81
Evgeny Lebedev <EvgenyLebedevv@gmail.com>
2023-03-17 14:47:08 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const btn = document.getElementById('myBtn')
+const modal = document.getElementById('myModal')
+
+btn.addEventListener('click', openModal)
+
+function openModal() {
+    modal.classList.add('open')
+    attachModalEvents()
+}
+
+function attachModalEvents() {
+    modal.querySelector('.close').addEventListener('click', closeModal)
+    document.addEventListener('keydown', handleEscape)
+    modal.addEventListener('click', handleOutside)
+}
+
+function closeModal() {
+    modal.classList.remove('open')
+    detachModalEvents()
+}
+
+function detachModalEvents() {
+    modal.querySelector('.close').removeEventListener('click', closeModal)
+    document.removeEventListener('keydown', handleEscape)
+    modal.removeEventListener('click', handleOutside)
+
+}
+
+function handleEscape(event) {
+    if (event.key === 'Escape') {
+        closeModal()
+    }
+}
+
+function handleOutside(event) {
+    !(event.target.closest('.modal-content')) ? closeModal() : false
+}
